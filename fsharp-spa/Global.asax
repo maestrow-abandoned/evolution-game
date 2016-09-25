7fcbd7c75540331962671550a8efe4a5ff380a8a
maestrow <andrey.shmelev@gmail.com>
2016-09-25 22:28:47 +0300

    fsharp-spa

---
+++ fsharp-spa/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="fsharp_spa.Global" %>
---
+++ fsharp-spa/Global.asax.fs
@@ -0,0 +1,7 @@
+namespace fsharp_spa
+
+type Global() =
+    inherit System.Web.HttpApplication()
+
+    member g.Application_Start(sender: obj, args: System.EventArgs) =
+        ()
