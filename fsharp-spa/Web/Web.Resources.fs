07a3942c2141f4c27b9790f8d7d3f23158ca5fc2
maestrow <andrey.shmelev@gmail.com>
2016-09-27 23:00:02 +0300

    Sortable jquery adapter

--- fsharp-spa/Web/Web.Resources.fs
+++ fsharp-spa/Web/Web.Resources.fs
@@ -4,8 +4,11 @@ open WebSharper
 open WebSharper.Core.Resources
 
 module Resources =
-  type Css() =
-    inherit BaseResource("styles.css")
+  type Css() = inherit BaseResource("styles.css")
+  type Sortable() = inherit BaseResource(@"paket-files\RubaXa\Sortable\Sortable.min.js")
+  type SortableJq() = inherit BaseResource(@"paket-files\RubaXa\Sortable\jquery.binding.js")
 
   [<assembly:Require(typeof<Css>)>]
+  [<assembly:Require(typeof<Sortable>)>]
+  [<assembly:Require(typeof<SortableJq>)>]
   do()
